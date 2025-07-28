5064023abbb4843d7446a9470dd5b0758814519e
Kellypops <123489367+Kellypops@users.noreply.github.com>
2025-07-28 08:33:50 +0200

    - Fixed some names to work with new unit convention, might need a rework...

--- modules/functions/functions_xrd.py
+++ modules/functions/functions_xrd.py
@@ -33,7 +33,7 @@ def xrd_get_integrated_from_hdf5(xrd_group, target_x, target_y):
 
     elif xrd_group.attrs["instrument"] == "Rigaku Smartlab":
         q_array = measurement_group["angle"][()]
-        intensity_array = measurement_group["intensity"][()]
+        intensity_array = measurement_group["counts"]
 
     else:
         raise KeyError(
