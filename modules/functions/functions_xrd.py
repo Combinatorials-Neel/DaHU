28de41c975502933b352c6b17e0d0713d182d740
Escouflenfer <rigautwilliam@gmail.com>
2026-07-24 10:59:12 +0200

    updated pyproject and fixed import in function_xrd

--- modules/functions/functions_xrd.py
+++ modules/functions/functions_xrd.py
@@ -10,7 +10,7 @@ from fabio import dtrekimage
 import pyFAI
 from scipy.signal import find_peaks
 
-from ..build.lib.hdf5_compilers.hdf5compile_base import rename_group
+from ..hdf5_compilers.hdf5compile_base import rename_group
 from ..functions.functions_shared import *
 from ..functions.functions_hdf5 import *
 
